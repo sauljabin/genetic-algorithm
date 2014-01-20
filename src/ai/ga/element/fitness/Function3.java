3a5cbf213119d8837153b1a7e089c426f2183924
Saúl Piña <sauljabin@gmail.com>
2014-01-19 18:36:26 -0530

    Mejora funcion 3

--- src/ai/ga/element/fitness/Function3.java
+++ src/ai/ga/element/fitness/Function3.java
@@ -34,7 +34,7 @@ public class Function3 extends Fitness {
 	@Override
 	public double getObjetiveValue(double... phenotype) {
 		double[] x = phenotype;
-		return 4 * Math.pow(x[0], 2) - 21 / 10 * Math.pow(x[0], 4) + 1 / 3 * Math.pow(x[0], 6) + x[0] * x[1] - 4 * Math.pow(x[1], 2) + 4 * Math.pow(x[1], 4);
+		return 4. * Math.pow(x[0], 2) - 21. / 10. * Math.pow(x[0], 4) + 1. / 3. * Math.pow(x[0], 6) + x[0] * x[1] - 4. * Math.pow(x[1], 2) + 4. * Math.pow(x[1], 4);
 	}
 
 }
